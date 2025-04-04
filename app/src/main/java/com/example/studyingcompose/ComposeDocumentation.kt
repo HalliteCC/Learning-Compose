9628aea0924a4a6c8c18dde87028d57e94ec8026
Caie Hallite <hallitecaie@gmail.com>
2025-04-04 15:10:05 -0300

    7th Commit - Theme and Color

--- app/src/main/java/com/example/studyingcompose/ComposeDocumentation.kt
+++ app/src/main/java/com/example/studyingcompose/ComposeDocumentation.kt
@@ -1,5 +1,7 @@
 package com.example.studyingcompose
 
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +26,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
@@ -47,14 +51,14 @@ fun OnboardingScreen(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     )
     {
         Text("Welcome to the Basic Codelab!")
         Button(
-            modifier = Modifier.padding(vertical = 24.dp),
+            modifier = modifier.padding(vertical = 24.dp),
             onClick = onContinueCliecked
         )
         {
@@ -69,7 +73,7 @@ fun Greatings(
     names: List<String> = List(1000) { "$it" }
 ) {
     LazyColumn(
-        modifier = Modifier.padding(vertical = 4.dp)
+        modifier = modifier.padding(vertical = 4.dp)
     ) {
         items(items = names) { name ->
             Greeting(name = name)
@@ -91,16 +95,22 @@ private fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
     Surface(
         color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+        shape = RoundedCornerShape(5.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
+        Row(modifier = modifier.padding(24.dp)) {
             Column(
-                modifier = Modifier
+                modifier = modifier
                     .weight(1f)
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text("Hello")
-                Text(name)
+                Text(
+                    name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(onClick = { expanded = !expanded }) {
                 Text(if (expanded) "Show Less" else "Show More")
--- app/src/main/java/com/example/studyingcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/studyingcompose/ui/theme/Theme.kt
@@ -9,17 +9,18 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BlueE8,
+    secondary = Color.Magenta,
+    tertiary = Color.Blue
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
+    primary = BlueE8,
+    secondary = Orange12D,
     tertiary = Pink40
 
     /* Other default colors to override
@@ -37,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 fun StudyingComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
